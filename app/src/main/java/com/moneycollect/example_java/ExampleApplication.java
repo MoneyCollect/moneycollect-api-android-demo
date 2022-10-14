382b7f96c9334f8dcb8d0de074d8a600c696293a
MoneyCollect <92731686+MoneyCollect@users.noreply.github.com>
2022-10-14 14:33:34 +0800

    Update ExampleApplication.java

--- app/src/main/java/com/moneycollect/example_java/ExampleApplication.java
+++ app/src/main/java/com/moneycollect/example_java/ExampleApplication.java
@@ -9,7 +9,7 @@ public class ExampleApplication extends Application {
     @Override
     public void onCreate() {
         //init MoneyCollectSdk
-       MoneyCollectSdk.init(this, "live_pr_97pxAhtkXCgm8Dd08L3NNTfV23hWRno9D9XZc9KnlK4","https://api.moneycollect.com")
+       MoneyCollectSdk.init(this, "live_pr_97pxAhtkXCgm8Dd08L3NNTfV23hWRno9D9XZc9KnlK4","https://api.moneycollect.com");
         super.onCreate();
     }
 }
